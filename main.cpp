f93e9270a9bd63a08b8c730c0cb2948c2fd04582
Saqib Kayani <m.s.kayani@edu.salford.ac.uk>
2015-04-17 08:00:14 +0100

    Removed unnecessary code that was commented out.

--- main.cpp
+++ main.cpp
@@ -9,28 +9,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
-//    foreach( QFileInfo drive, QDir::drives() )
-//    {
-//    qDebug() << "Drive: " << drive.absolutePath();
-//    }
-
-//    QSplitter *splitter = new QSplitter;
-//    QFileSystemModel *imageModel = new QFileSystemModel;
-//    imageModel->setRootPath(QDir::rootPath());
-
-//    QListView *list = new QListView(splitter);
-//    list->setModel(imageModel);
-//    list->setRootIndex(imageModel->index(QDir::rootPath()));
-
-//    QTreeView *tree = new QTreeView(splitter);
-//    tree->setModel(imageModel);
-//    tree->setRootIndex(imageModel->index(QDir::rootPath()));
-
     PhotoManager w;
     w.show();
-
-//    splitter->setWindowTitle("Photo Manager");
-//    splitter->show();
     return a.exec();
 }
